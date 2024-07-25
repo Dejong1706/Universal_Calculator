9790b18d059cae880841a6854601d99f137d0422
JanBe <ppk1998@naver.com>
2024-07-25 13:09:44 +0900

    axios 비활성화

--- src/components/CountryFirstSelection.tsx
+++ src/components/CountryFirstSelection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { exchangeRateData, exchangeRateName, nationalFlag } from "@/data/data";
 import CountrySelectionModal from "./CountrySelectionModal";
 import FirstCountry from "./FirstCountry";
